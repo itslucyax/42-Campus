83472f861936720c0f10686ed4702bfce0448784
Lucia Martin Eslava <luciamar@c4r2s3.42urduliz.com>
2026-01-17 13:17:42 +0100

    Added ft_memcmp.c

--- level-0/libft/ft_memcmp.c
+++ level-0/libft/ft_memcmp.c
@@ -6,7 +6,7 @@
 /*   By: luciamar <luciamar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/17 12:52:25 by luciamar          #+#    #+#             */
-/*   Updated: 2026/01/17 12:55:05 by luciamar         ###   ########.fr       */
+/*   Updated: 2026/01/17 13:16:37 by luciamar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,8 +15,24 @@
 ** Compara los primeros bytes de s1  ys2
 ** Comapara byte por byte por unsigned char
 **
+** https://github.com/nakamavg/libft?tab=readme-ov-file#ft_memcmp
+** https://github.com/vittoric/Libft/blob/master/ft_memcmp.c
 */
 int	ft_memcmp(const void *s1, const void *s2, size_t n)
 {
-	
+	unsigned const char	*str1;
+	unsigned const char	*str2;
+	size_t				i;
+
+	*str1 = (const unsigned char *)s1;
+	*str2 = (const unsigned char *)s2;
+	i = 0;
+	while (n < i)
+	{
+		if (*str1 != *str2)
+			return ((int)(*str1 - *str2));
+	}
+	*str1++;
+	*str2++;
+	i++;
 }
