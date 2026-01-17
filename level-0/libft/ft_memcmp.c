825bb002a11438189a15518c43d2ee6008123e3e
Lucia Martin Eslava <luciamar@c4r2s3.42urduliz.com>
2026-01-17 12:58:52 +0100

    plus2 fts

---
+++ level-0/libft/ft_memcmp.c
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memcmp.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: luciamar <luciamar@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/01/17 12:52:25 by luciamar          #+#    #+#             */
+/*   Updated: 2026/01/17 12:55:05 by luciamar         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+/* ft_memcmp - Compara 2 bloques de memoria
+** Compara los primeros bytes de s1  ys2
+** Comapara byte por byte por unsigned char
+**
+*/
+int	ft_memcmp(const void *s1, const void *s2, size_t n)
+{
+	
+}
